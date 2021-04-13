09a5e66ff6287602463d825d1162138f385306e4
seaona <mariona@gmx.es>
2021-04-13 12:22:15 +0200

    FEATURE: access control contracts updated with roles functions

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -3,7 +3,21 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      websockets: true
+    }
+  },
+  compilers: {
+    solc: {
+       version: "^0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
     }
   }
 };
